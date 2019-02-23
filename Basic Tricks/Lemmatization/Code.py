88ebd5e7fed3fd0da433a50b5a4cf06acf2352e1
Hafiz Muhammad Sheharyar <sheharyar6@live.com>
2019-02-24 01:57:47 +0500

    Update Code.py
    Added 'verb' switching as the default POS is nouns.

--- Basic Tricks/Lemmatization/Code.py
+++ Basic Tricks/Lemmatization/Code.py
@@ -3,3 +3,6 @@
 from nltk.stem import WordNetLemmatizer
 lemmatizer = WordNetLemmatizer()
 print(lemmatizer.lemmatize('processes'))
+
+//The default POS is nouns. Switch it to verb for better results.
+print(lemmatizer.lemmatize('running', pos="v"))
