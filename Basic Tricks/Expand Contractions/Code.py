be9137b1d758e231b953ec70df2e8c4e0d3c5c0e
Hafiz Muhammad Sheharyar <sheharyar6@live.com>
2019-02-24 02:46:21 +0500

    Create Code.py

---
+++ Basic Tricks/Expand Contractions/Code.py
@@ -0,0 +1,44 @@
+<=============================================== Expand Contractions ===============================================>
+
+<--------First Method-------->
+from contractions import CONTRACTION_LIST
+def decontract(text, contraction_mapping=CONTRACTION_LIST):
+    
+    contractions_pattern = re.compile('({})'.format('|'.join(contraction_mapping.keys())), 
+                                      flags=re.IGNORECASE|re.DOTALL)
+    def expand_match(contraction):
+        match = contraction.group(0)
+        first_char = match[0]
+        expanded_contraction = contraction_mapping.get(match)\
+                                if contraction_mapping.get(match)\
+                                else contraction_mapping.get(match.lower())                       
+        expanded_contraction = first_char+expanded_contraction[1:]
+        return expanded_contraction
+        
+    expanded_text = contractions_pattern.sub(expand_match, text)
+    expanded_text = re.sub("'", "", expanded_text)
+    return expanded_text
+
+decontract("I'd try to expand the contractions. You'll see the results with the code I've written.")
+
+
+<--------Second Method-------->
+import re
+def decontract(phrase):
+    # specific
+    phrase = re.sub(r"won\'t", "will not", phrase)
+    phrase = re.sub(r"can\'t", "can not", phrase)
+
+    # general
+    phrase = re.sub(r"n\'t", " not", phrase)
+    phrase = re.sub(r"\'re", " are", phrase)
+    phrase = re.sub(r"\'s", " is", phrase)
+    phrase = re.sub(r"\'d", " would", phrase)
+    phrase = re.sub(r"\'ll", " will", phrase)
+    phrase = re.sub(r"\'t", " not", phrase)
+    phrase = re.sub(r"\'ve", " have", phrase)
+    phrase = re.sub(r"\'m", " am", phrase)
+    return phrase
+
+test = "I'd try to expand the contractions. You'll see the results with the code I've written."
+print(decontract(test))
